923ad093db111eda3f960ff6bdcae6318ebec68b
Maadhu938 <maadhuavati12@gmail.com>
2026-04-23 11:50:17 +0530

    Update CORS to allow frontend custom domain

--- backend/app.py
+++ backend/app.py
@@ -15,7 +15,7 @@ from datetime import datetime
 port = int(os.environ.get('PORT', 5000))
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["https://cryptoapp.maadhuavati.in"])
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 _model_cache = {}
